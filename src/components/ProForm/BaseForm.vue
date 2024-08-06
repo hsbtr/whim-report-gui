75774ddc076910b3b25219287b89276ccd28d6bd
hsbtr <hsbtr@qq.com>
2024-08-06 19:34:59 +0800

    feat: 表单组件二次封装初步尝试

---
+++ src/components/ProForm/BaseForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, provide } from 'vue';
+import { NForm } from 'naive-ui';
+
+const formValue = ref();
+provide('formValue', formValue);
+
+</script>
+
+<template>
+  <n-form></n-form>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProForm/ProFormSelect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NFormItem, NSelect } from 'naive-ui';
+import type { FormItemProps, SelectProps } from 'naive-ui';
+
+type ProFormSelectProps = FormItemProps & { filedProps: SelectProps };
+
+const props = defineProps<ProFormSelectProps>();
+
+</script>
+
+<template>
+  <n-form-item :label="props.label" :path="props.path" >
+    <n-select />
+  </n-form-item>
+</template>
+
+<style scoped>
+
+</style>
