f0a85950094953f2946898ace0564fdddf6d6578
hsbtr <hsbtr@qq.com>
2025-05-28 14:21:12 +0800

    fix: 修复一个声明

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@ import AppProvider from '@/components/popups/AppProvider.vue';
 import { useUiStore } from '@/stores/uiStore';
 import { LanguageEnum } from '@/config';
 import { useHLJSInit } from '@/hooks';
-import type { GlobalTheme } from 'naive-ui';
+import type { ConfigProviderProps } from 'naive-ui';
 
 const uiStore = useUiStore();
 const hlJS = useHLJSInit();
 
-const dark = computed(() => {
+const dark = computed<ConfigProviderProps['theme']>(() => {
   return uiStore.getTheme === 'dark' ? darkTheme : null;
-}) as GlobalTheme;
+});
 const locale = computed(() => {
   return uiStore.getLang === LanguageEnum.ZH ? zhCN : enUS;
 });
@@ -28,7 +28,7 @@ const dateLocale = computed(() => {
     :theme="dark"
     :locale="locale"
     :date-locale="dateLocale"
-    :hljs="hlJS as any"
+    :hljs="hlJS"
   >
     <AppProvider>
       <router-view />
